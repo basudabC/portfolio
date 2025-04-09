a5ff6e96319a50a1049816d9ec35d89621274e5b
Basudab Chowdhury <basudab.chowdhory@gmail.com>
2025-04-09 17:17:57 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          "corp-blue": "#1E3A8A",
-          "corp-gold": "#D4AF37",
-        },
-        fontFamily: {
-          roboto: ["Roboto", "sans-serif"],
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "corp-blue": "#1E3A8A",
+        "corp-gold": "#D4AF37",
+        "corp-gray": "#F3F4F6",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
